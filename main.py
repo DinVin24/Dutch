91441198d76955f3405846ca4712b2a37a5bfde4
DinVin24 <emisoltuzu@gmail.com>
2025-09-30 21:43:28 +0300

    Dealing cards

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 values = ['ace', 'two', 'three', 'four', 'five',
           'six', 'seven', 'eight', 'nine', 'ten',
           'jack', 'queen', 'king']
@@ -6,7 +8,24 @@ class Card:
     def __init__(self, value, suit):
         self.value = value
         self.suit = suit
+class Player:
+    def __init__(self):
+        self.cards=[]
 
 deck = [Card(value,suit) for value in values for suit in suits ]
+random.shuffle(deck)
+
+NrPlayers = 4
+Players = [Player() for i in range(NrPlayers)]
+for p in Players:
+    for i in range(4):
+        p.cards.append(deck.pop())
+
+for p in Players:
+    for card in p.cards:
+        print(card.value, card.suit)
+    print()
+
+
 for card in deck:
-    print(card.value,card.suit)
+    print(card.value, card.suit)
