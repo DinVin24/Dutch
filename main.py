fef74de9cdf7efcf1f2a66df38d7d8a507f18bd7
DinVin24 <emisoltuzu@gmail.com>
2025-09-30 22:34:38 +0300

    choosing cards

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ values = ['ace', 'two', 'three', 'four', 'five',
           'six', 'seven', 'eight', 'nine', 'ten',
           'jack', 'queen', 'king']
 suits = ['spade', 'clubs', 'heart', 'diamond']
+
 class Card:
     def __init__(self, value, suit):
         self.value = value
         self.suit = suit
+
 class Player:
     def __init__(self):
         self.cards=[]
@@ -19,13 +21,33 @@ NrPlayers = 4
 Players = [Player() for i in range(NrPlayers)]
 for p in Players:
     for i in range(4):
-        p.cards.append(deck.pop())
+        p.cards.append(deck.pop(0))
 
 for p in Players:
     for card in p.cards:
         print(card.value, card.suit)
     print()
 
+for i in range(NrPlayers):
+    choice = [int (x) - 1 for x in input(f"Player {i+1}, choose 2 cards to see:\n1 2 3 4\n").split()]
+    print(Players[i].cards[choice[0]].value, Players[i].cards[choice[0]].suit)
+    print(Players[i].cards[choice[1]].value, Players[i].cards[choice[1]].suit)
 
-for card in deck:
-    print(card.value, card.suit)
+
+
+#
+# GameStatus = 1
+# while GameStatus == 1:
+#     for i in range(NrPlayers):
+#         print(deck[0].value, deck[0].suit)
+#
+
+
+# for p in Players:
+#     for card in p.cards:
+#         print(card.value, card.suit)
+#     print()
+#
+#
+# for card in deck:
+#     print(card.value, card.suit)
